8f852da296d000f7534f554b8e2b967274e50e80
Lister <andrew.lister@stfc.ac.uk>
2019-02-28 18:17:52 +0000

    repr

--- create_slideshow.py
+++ create_slideshow.py
@@ -6,3 +6,6 @@ class Photo:
         self.orientation = orientation
         self.num = num
         self.tags = tags
+
+    def __repr__(self):
+        return "Photo ({}): {}".format(self.orientation,self.tags) 
