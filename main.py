c58189c957267d4f65ad072baeb68d0698385f77
Lister <andrew.lister@stfc.ac.uk>
2019-03-01 15:21:39 +0000

    included timer and made robust against empty inputs

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from file_handler import read_input, write_output, read_params
 from create_slideshow import sort_photos
 from greedy import do_greedy
-from random import make_random
+import numpy as np
+import time
 
 files = ['a_example.txt', 'b_lovely_landscapes.txt', 'c_memorable_moments.txt',
          'd_pet_pictures.txt', 'e_shiny_selfies.txt']
@@ -11,19 +12,22 @@ greed = read_params()
 
 def main():
 
-    for file in files:
+    for f in files:
         # Create the slideshow
-        pics = read_input(file)
+        t = time.time()
+        pics = read_input(f,1)
+        if pics!=[]:
+                np.random.shuffle(pics)
+                # Sort photos
+                slides = sort_photos(pics)
 
-        # Sort photos
-        slides = sort_photos(pics)
-
-        #slides = do_greedy(slides, greed)
-        slides = make_random(slides)
-
-        # Write the slideshow to file
-        write_output(file, slides)
-
+                slides = do_greedy(slides, greed)
+                
+                # Write the slideshow to file
+                write_output(f, slides)
+                
+        t = time.time()-t
+        print('{} done. ({}mins, {:.3}s)'.format(f,t//60,t%60))
 
 if __name__ == '__main__':
     main()
