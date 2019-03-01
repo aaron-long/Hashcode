b008f1dedd921c49f4884737429299a1d61d72ab
Lister <andrew.lister@stfc.ac.uk>
2019-03-01 15:23:00 +0000

    Switched to using deque Optimisation now done in batches

--- greedy.py
+++ greedy.py
@@ -1,5 +1,6 @@
 from create_slideshow import *
 import numpy as np
+from collections import deque
 
 def calc_score(slide1, slide2):
     tags1 = sorted(slide1.tags)
@@ -33,34 +34,51 @@ def calc_score(slide1, slide2):
     return min([matching,distinct1,distinct2])
 
 def take_best(start, slides,num = 1000):
-    best_score = 0
-    index = 0
-    crap_slides = []
-    for j,tmp in enumerate(slides[:num]):
+    tmp = slides.pop()
+    i = tmp[0]
+    s = tmp[1]
+    best_score = calc_score(start,s)
+    best_index = i
+    best_slide = s
+    crap_slides = deque([],maxlen=len(slides)-1)
+    while((len(crap_slides)<num) & (len(slides)>0)):
+        tmp = slides.pop()
         i = tmp[0]
         s = tmp[1]
         score = calc_score(start,s)
         if score>best_score:
-            crap_slides+=[slides[index]]
+            crap_slides.append((best_index,best_slide))
             best_score = score
-            index = j
+            best_slide = s
+            best_index = i
 
         else:
-            crap_slides +=[(i,s)]
-    #print(slides[index][0])
-    return slides[index][0],crap_slides+slides[num:]
+            crap_slides.append((i,s))
+    crap_slides.extend(slides)
+    return best_index,best_slide,crap_slides
 
-def do_greedy(slides,num = 100):
-    to_process = list(enumerate(slides))
-    order = [0]
-    while(len(to_process) > 1):
-        i,s = take_best(to_process[0][1],to_process[1:],num)
-        order+=[i]
-        to_process = s
+def do_greedy(slides,block_size = 100):
+    blocks = [deque([(n*block_size+i,j) 
+                     for i,j in enumerate(slides[n*block_size:(n+1)*block_size])
+                    ]) 
+              for n in range(len(slides)//block_size)
+             ]
+    order = deque([],maxlen=len(slides))
+    for to_process in blocks:
+        tmp = to_process.pop()
+        order.append(tmp[0])
+        curr = tmp[1]
+        while(len(to_process) > 1):
+            i,s,p = take_best(curr,to_process,block_size)
+            order.append(i)
+            curr = s
+            to_process = p
 
     out = []
-    for i in order:
-        out+=[slides[i]]
+    while(len(order)>0):
+        out+=[slides[order.popleft()]]
+    print(out[:10])
+    
     return out
 
 
